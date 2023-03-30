edc81c9f92a20463bec41366161d0ec0f996050a
HantaAkai <hantaakai@gmail.com>
2023-03-30 20:59:30 +0200

    Build Unit rotation while moving using correct Lerp.

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -4,19 +4,44 @@ using UnityEngine;
 
 public class Unit : MonoBehaviour {
 
+    private const string IS_WALKING = "IsWlaking";
+
+    [SerializeField] private Animator unitAnimator; 
+
     private Vector3 targetPosition;
+    private Vector3 startingLerpPosition;
+    private float currentLerpTime = 0;
+    private float maxLerpTime = 1;
+
     private float moveSpeed = 4f;
 
+  
     private void Update() {
 
         float stoppingDistance = .1f;
         if (Vector3.Distance(transform.position, targetPosition) > stoppingDistance) {
-        Vector3 moveDirection = (targetPosition - transform.position).normalized;
-        transform.position += moveDirection * Time.deltaTime * moveSpeed;
+            Vector3 moveDirection = (targetPosition - transform.position).normalized;
+            transform.position += moveDirection * Time.deltaTime * moveSpeed;
+
+            //This Lerp can be changed into miss use
+            //transform.forward = Vector3.Lerp(transform.forward, moveDirection, Time.deltaTime * rotationSpeed);
+            //TODO: Might want to look at transform.rotation or transform.eulerAngles to make the rotation time fixed
+            float rotationSpeed = 2f;
+            float rotationPercentage = currentLerpTime / maxLerpTime;
+            transform.forward = Vector3.Lerp(startingLerpPosition, moveDirection, rotationPercentage * rotationSpeed);
+            currentLerpTime += Time.deltaTime;
+
+            unitAnimator.SetBool(IS_WALKING, true);
+        } else {
+            unitAnimator.SetBool (IS_WALKING, false);
+            startingLerpPosition = transform.forward;
+            currentLerpTime = 0;
         }
 
         if (Input.GetMouseButtonDown(0)) {
             Move(MouseWorld.GetPosition());
+            startingLerpPosition = transform.forward;
+            currentLerpTime = 0;
         }
     }
 
