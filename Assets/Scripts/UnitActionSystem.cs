db06faf1b0f19a1b3a94a9732841e9d9a7235e3c
HantaAkai <hantaakai@gmail.com>
2023-09-17 19:25:23 +0200

    Build unit selection visuals.

--- Assets/Scripts/UnitActionSystem.cs
+++ Assets/Scripts/UnitActionSystem.cs
@@ -1,25 +1,54 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class UnitActionSystem : MonoBehaviour {
 
+    public static UnitActionSystem Instance { get; private set; }
+
+    public event EventHandler OnSelectedUnitChanged; 
+
     [SerializeField] private Unit selectedUnit;
     [SerializeField] private LayerMask unitLayerMask;
 
+    private void Awake() {
+        if (Instance != null) {
+            Debug.LogError("There is more than one UnitActionSystem! " + transform + " - " + Instance);
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+    }
+
     private void Update() {
         if (Input.GetMouseButtonDown(0)) {
-            HandleUnitSelection();
+            if (TryHandleUnitSelection()) {
+                return;
+            }
+
             selectedUnit.Move(MouseWorld.GetPosition());
         }
     }
 
-    private void HandleUnitSelection() {
+    private bool TryHandleUnitSelection() {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         if (Physics.Raycast(ray, out RaycastHit raycastHit, float.MaxValue, unitLayerMask)) {
             if(raycastHit.transform.TryGetComponent<Unit>(out Unit unit)) {
-                selectedUnit = unit;
+                SetSelectedUnit(unit);
+                return true;
             }
         }
+
+        return false;
+    }
+
+    private void SetSelectedUnit(Unit unit) {
+        selectedUnit = unit;
+        OnSelectedUnitChanged?.Invoke(this, EventArgs.Empty);
+    }
+
+    public Unit GetSelectedUnit() {
+        return selectedUnit;
     }
 }
