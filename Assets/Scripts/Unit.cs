bf7df0233f670608a42130f000fcc4b8643c1919
HantaAkai <hantaakai@gmail.com>
2023-09-17 18:19:48 +0200

    Build Animations aligned with Move.

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Unit : MonoBehaviour {
-
+    [SerializeField] private Animator unitAnimator;
+    
     private Vector3 targetPosition;
 
     private void Update() {
@@ -13,11 +14,18 @@ public class Unit : MonoBehaviour {
             Vector3 moveDirection = (targetPosition - transform.position).normalized;
             float moveSpeed = 4f;
             transform.position += moveDirection * moveSpeed * Time.deltaTime;
+
+            float rotateSpeed = 10f;
+            transform.forward = Vector3.Lerp(transform.forward, moveDirection, Time.deltaTime * rotateSpeed);
+
+            unitAnimator.SetBool("IsWalking", true);
+        } else {
+            unitAnimator.SetBool("IsWalking", false);
         }
 
 
-        if (Input.GetKeyDown(KeyCode.T)) {
-            Move(new Vector3(4, 0, 4));
+        if (Input.GetMouseButtonDown(0)) {
+            Move(MouseWorld.GetPosition());
         }
     }
 
