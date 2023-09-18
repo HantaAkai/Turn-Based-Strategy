def29b7c0b2a645780face059cb9b063929ebfeb
HantaAkai <hantaakai@gmail.com>
2023-09-18 22:01:08 +0200

    Start building gridSystem. Build class and conversion methods.

---
+++ Assets/Scripts/Testing.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testing : MonoBehaviour {
+
+    private GridSystem gridSystem;
+
+    private void Start() {
+        gridSystem = new GridSystem(10, 10, 2f);
+
+        Debug.Log(new GridPosition(5,7));
+    }
+
+    private void Update() {
+        Debug.Log(gridSystem.GetGridPosition(MouseWorld.GetPosition()));
+    }
+}
