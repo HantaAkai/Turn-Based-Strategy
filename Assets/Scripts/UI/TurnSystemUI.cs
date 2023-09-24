43c50dd2f080149b6bb0e4560e67473726342dcf
HantaAkai <hantaakai@gmail.com>
2023-09-24 13:23:57 +0200

    Build EnemyTurnUI.

--- Assets/Scripts/UI/TurnSystemUI.cs
+++ Assets/Scripts/UI/TurnSystemUI.cs
@@ -8,6 +8,7 @@ public class TurnSystemUI : MonoBehaviour {
 
     [SerializeField] private Button endTurnButton;
     [SerializeField] private TextMeshProUGUI turnNumberText;
+    [SerializeField] private GameObject enemyTurnVisualGameObject;
 
     private void Start() {
         endTurnButton.onClick.AddListener(() => { 
@@ -17,15 +18,21 @@ public class TurnSystemUI : MonoBehaviour {
         TurnSystem.Instance.OnTurnChanged += TurnSystem_OnTurnChanged;
 
         UpdateTurnText();
+        UpdateEnemyTurnVisual();
     }
 
     private void TurnSystem_OnTurnChanged(object sender, System.EventArgs e) {
         UpdateTurnText();
+        UpdateEnemyTurnVisual(); 
     }
 
     private void UpdateTurnText() {
         int turnNumber = TurnSystem.Instance.TurnNumber;
         turnNumberText.text = $"TURN {turnNumber}";
+    }
+
+    private void UpdateEnemyTurnVisual() {
+        enemyTurnVisualGameObject.SetActive(!TurnSystem.Instance.IsPlayerTurn);
     }
 
 }
