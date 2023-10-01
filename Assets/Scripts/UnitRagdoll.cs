822a93d4ee153f94461c2542bfedf62a44050f93
HantaAkai <hantaakai@gmail.com>
2023-10-01 17:21:25 +0200

    Add explosion to unit ragdoll on unit death.

--- Assets/Scripts/UnitRagdoll.cs
+++ Assets/Scripts/UnitRagdoll.cs
@@ -9,6 +9,9 @@ public class UnitRagdoll : MonoBehaviour
 
     public void Setup(Transform originalRootBone) {
         MatchAllChildTranforms(originalRootBone, ragdollRootBone);
+
+        //TODO: Pass bulletProjectile hit point instead of transform.position
+        ApplyExplosionToRagdoll(ragdollRootBone, 300f, transform.position , 10f);
     }
 
     private void MatchAllChildTranforms(Transform root, Transform clone) {
@@ -20,6 +23,16 @@ public class UnitRagdoll : MonoBehaviour
 
                 MatchAllChildTranforms (child, cloneChild);
             }
+        }
+    }
+
+    private void ApplyExplosionToRagdoll(Transform root, float explosionForce, Vector3 explosionPosition, float explosionRange) {
+        foreach (Transform child in root) {
+            if (child.TryGetComponent<Rigidbody>(out Rigidbody childRigidbody)) {
+                childRigidbody.AddExplosionForce(explosionForce, explosionPosition, explosionRange);
+            }
+
+            ApplyExplosionToRagdoll(child, explosionForce, explosionPosition, explosionRange);
         }
     }
 }
