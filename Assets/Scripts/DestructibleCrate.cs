aec26d2b2ff1076134e2e15d58dc84040d584277
HantaAkai <hantaakai@gmail.com>
2023-10-15 15:13:00 +0200

    Add destroyed state and FX for crate.

--- Assets/Scripts/DestructibleCrate.cs
+++ Assets/Scripts/DestructibleCrate.cs
@@ -7,6 +7,8 @@ public class DestructibleCrate : MonoBehaviour {
 
     public static event EventHandler OnAveDestroyed;
 
+    [SerializeField] private Transform crateDestroyedPrefab;
+
     public GridPosition gridPosition { get; private set; }
 
     private void Start() {
@@ -14,8 +16,22 @@ public class DestructibleCrate : MonoBehaviour {
     }
 
     public void TakeDamage() {
+        Transform crateDestroyedTransform = Instantiate(crateDestroyedPrefab, transform.position, transform.rotation);
+
+        ApplyExplosionToChildren(crateDestroyedTransform, 150f, transform.position, 10f);
+
         Destroy(gameObject);
 
         OnAveDestroyed?.Invoke(this, EventArgs.Empty);
+    }
+
+    private void ApplyExplosionToChildren(Transform root, float explosionForce, Vector3 explosionPosition, float explosionRange) {
+        foreach (Transform child in root) {
+            if (child.TryGetComponent<Rigidbody>(out Rigidbody childRigidBody)) {
+                childRigidBody.AddExplosionForce(explosionForce, explosionPosition, explosionRange);
+            }
+
+            ApplyExplosionToChildren(child, explosionForce, explosionPosition, explosionRange);
+        }
     }
 }
